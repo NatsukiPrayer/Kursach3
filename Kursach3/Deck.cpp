cec515930366151c8ea0811922cc5e454905aeab
NatsukiPrayer <aloneandtired@yandex.ru>
2019-05-30 01:55:12 +0300

    Added vectors

--- Kursach3/Deck.cpp
+++ Kursach3/Deck.cpp
@@ -4,6 +4,24 @@
 #include <random>
 
 
+/*void Deck::deck_init()
+{
+	std::vector <Cards> deck_;
+	for (int i = 0; i < 9; i++) {
+		deck_[i]=Cards(i, true, true);
+	}
+	for (int i = 8; i < 18; i++) {
+		deck[i] = Cards((i - 9), true, false);
+	}
+	for (int i = 18; i < 27; i++) {
+		deck[i] = Cards((i - 18), false, true);
+	}
+	for (int i = 27; i < 36; i++) {
+		deck[i] = Cards((i - 27), false, false);
+	}
+	this->deck =
+}*/
+
 void Deck::shuffle()
 {
 	auto rng = std::default_random_engine{};
--- Kursach3/Deck.h
+++ Kursach3/Deck.h
@@ -6,5 +6,6 @@ class Deck : public Cards {
 private:
 	std::vector <Cards> deck;
 public:
+	void deck_init();
 	void shuffle();
 };
